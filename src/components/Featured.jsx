ca5473a76da7953d565341c1f9a132273f36a13a
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-11-30 23:48:41 +0600

    fasf
    sdf

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -36,13 +36,13 @@ const getCat = (num, terms) =>{
         <div className="row g-3">
 
 <Splide  options={{
-  perPage    : 6,
+  perPage    : 8,
   gap        : 0,
   pagination : false,
   arrows : true,
   breakpoints: {
     1200: { perPage: 5, gap: 0 },
-    640 : { gap: 0 , perPage: 2},
+    640 : { gap: 0 , perPage: 3},
   },
 }}>
         { pro?.map((product, index) => (
